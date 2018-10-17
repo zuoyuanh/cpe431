5446acd44be006624ffb21b8d704515c880c1e0c
zuoyuanh <zuhuang@unix1.csc.calpoly.edu>
2018-10-16 17:09:18 -0700

    bug fix for scanf

--- llvm/LLVMReadExpressionType.java
+++ llvm/LLVMReadExpressionType.java
@@ -4,6 +4,9 @@ public class LLVMReadExpressionType implements LLVMType
 {
    public String getReadInstructionString(String typeRep, String opnd)
    {
+   	  if (opnd.charAt(0) == 'u') {
+   	  	 opnd = "%" + opnd;
+   	  }
       return "call i32 (i8*, ...)* @scanf(i8* getelementptr inbounds ([4 x i8]* @.read, i32 0, i32 0), " + typeRep + "* " + opnd + ")\n";
    }
 }
