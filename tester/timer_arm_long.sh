5697985cb6787dd2765f838e5e46b6612f63bd50
zuoyuanh <zuhuang@unix1.csc.calpoly.edu>
2018-12-12 21:40:10 -0800

    bug fix

---
+++ tester/timer_arm_long.sh
@@ -0,0 +1,36 @@
+echo "Timer"
+echo "============================================="
+
+cat test_cases_names.txt | while read filename
+do
+   cd benchmarks/$filename
+
+   echo "[Test case $filename]"
+   
+   gcc -O0 $filename.c -o $filename.gcc.no_o.out
+   gcc -O3 $filename.c -o $filename.gcc.out
+   gcc -c $filename.s
+   gcc -c $filename.no_o.s
+   gcc $filename.o -o $filename.out
+   gcc $filename.no_o.s -o $filename.no_o.out
+
+   echo "time for optimized ARM: " 
+   time $(./$filename.out < ../../benchmarks/$filename/input.longer > $filename.myout.longer) 
+   echo "--------------"
+   echo "time for non-optimized ARM: " 
+   time $(./$filename.no_o.out < ../../benchmarks/$filename/input.longer > $filename.myout.longer) 
+   echo "--------------"
+   echo "time for optimized GCC: " 
+   time $(./$filename.gcc.out < ../../benchmarks/$filename/input.longer > $filename.myout.longer) 
+   echo "--------------"
+   echo "time for non-optimized GCC: " 
+   time $(./$filename.gcc.no_o.out < ../../benchmarks/$filename/input.longer > $filename.myout.longer) 
+   echo "--------------"
+
+   echo
+   echo
+   echo
+
+   cd ../..
+
+done
